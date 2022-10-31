bd29db403b3091d18bf1398674d1bfbe987040d3
stenlyadam <stenly.adam@unklab.ac.id>
2022-10-31 09:21:40 +0800

    update index.js

--- tokyo/index.js
+++ tokyo/index.js
@@ -1,3 +1,4 @@
+console.log("Test Fix Conflict");
 console.log("Group Exercises #2 (Tokyo)");
 /* Anggota : 
 -   Kilis, Vannesa Zevania
@@ -15,7 +16,7 @@ let people = ["Greg", "Mary", "Devon", "James"];
 //No.1 Using a for-loop, iterate through this array and console.log all of the people.
 for (let i = 0; i < people.length; i++) {
   console.log("Ini iterasi dipakai forLoop dengan nama : ", people[i]);
-};
+}
 
 //No.2 Using a forEach(),  iterate through this array and console.log all of the people.
 people.forEach(function (item, index) {
@@ -42,7 +43,7 @@ console.log(people);
 for (let i = 0; i < people.length; i++) {
   console.log(people[i]);
   if (people[i] === "Mary") {
-      break;
+    break;
   }
 }
 
@@ -59,28 +60,23 @@ console.log(people);
 let withBob = people.concat("Bob");
 console.log(withBob);
 
-
-
 console.log("Array Exercises");
 
 let programming = {
-
   languages: ["JavaScript", "Python", "Ruby"],
   isChallenging: true,
   isRewarding: true,
   difficulty: 8,
   jokes:
     "http://stackoverflow.com/questions/234075/what-is-your-best-programmer-joke",
-
 };
 
 //No.1 Write the command to add the language "Go" to the end of the languages array
 programming.languages.push("GO");
 console.log(programming.languages);
 
-
 //No.2 By using the bracket notation, change the difficulty to the value of 7
-programming["difficulty"] = 7;                                                                                
+programming["difficulty"] = 7;
 console.log(programming);
 
 //No.3 Using the delete keyword, write the command to remove the jokes key from the programming object
@@ -92,10 +88,8 @@ programming.isFun = true;
 console.log(programming);
 
 //No.5 Using a map(), iterate through the languages array and update each element to be "0 - JavaScript, 1 - Python, ... ",
-//where 0 represents the index of the array, and console.log the output. 
+//where 0 represents the index of the array, and console.log the output.
 let outputLang = programming.languages.map(function (item, index) {
-
   return index + " - " + item;
-  
 });
 console.log(outputLang);
